b81353eee97ccad0c0dc6945ffbc2e0fc6218445
Kadzzii <asbudin@edu.hse.ru>
2022-11-29 11:48:59 +0500

    create account templates

--- smartfridge/models.py
+++ smartfridge/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    product_type = models.CharField(max_length=50)
+    bar_code = models.IntegerField()
+
+
+class ListOfProduct(models.Model):
+    id_user = models.ForeignKey()
+    id_product = models.ForeignKey()
+    price = models.IntegerField()
+    date_purchase = models.DateField()
+    quantity = models.IntegerField()
+
+
+class ProductShelfDate(models.Model):
+    id_product = models.ForeignKey()
