1748d29132bfde33414a5b12e5bad49e7bdddcec
Artem <asbudin@edu.hse.ru>
2022-11-30 12:42:30 +0500

    create scaner template

--- smartfridge/urls.py
+++ smartfridge/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from smartfridge.views import home
+from smartfridge.views import home, scaner
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('/scan', scaner, name='scaner')
+
 ]
--- smartfridge/views.py
+++ smartfridge/views.py
@@ -6,7 +6,8 @@ def home(request):
     products = ListOfProduct.objects.all().filter(id_user = request.user.id)
     return render(request, 'home.html', {'product_list': products})
 
-
+def scaner(request):
+    return render(request, 'scan.html')
 
 
 # Create your views here.
